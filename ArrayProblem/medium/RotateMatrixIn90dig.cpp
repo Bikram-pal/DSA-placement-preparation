e687aa9aef8432360ba764dbb62307290350993f
Bikram Pal <palbikram557@gmail.com>
2026-05-27 20:28:47 +0530

    fix: correct implementation of optimal Rotate Matrix function and clean up output

--- ArrayProblem/medium/RotateMatrixIn90dig.cpp
+++ ArrayProblem/medium/RotateMatrixIn90dig.cpp
@@ -15,6 +15,32 @@ vector<vector<int>> rotateMatrix(vector<vector<int>> &matrix, int n)
   return ans;
 }
 
+
+// optimal solution -> to reduce sc 
+void rotateMatrix_optimal(vector<vector<int>> &matrix, int n)
+{
+  for(int i=0; i<n; i++)
+  {
+    for(int j=i+1; j<n; j++)
+    {
+      else
+      {
+        swap(matrix[i][j], matrix[j][i]);
+      }
+    }
+  }
+  for(int i=0; i<n; i++)
+  {
+    int k = 0;
+    int j = n-1;
+    while(k<j)
+    {
+      swap(matrix[i][j], matrix[i][k]);
+      k++;
+      j--;
+    }
+  }
+}
 int main()
 {
   vector<vector<int>> matrix = {{5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16}};
@@ -29,12 +55,21 @@ int main()
     cout<<endl;
   }
   cout<<endl;
-  ans = rotateMatrix(matrix, n);
+  // ans = rotateMatrix(matrix, n); //brute force
+  // for(int i=0; i<n; i++)
+  // {
+  //   for(int j=0; j<n; j++)
+  //   {
+  //     cout<< ans[i][j]<<" ";
+  //   }
+  //   cout<<endl;
+  // }
+  rotateMatrix_optimal(matrix, n);
   for(int i=0; i<n; i++)
   {
     for(int j=0; j<n; j++)
     {
-      cout<< ans[i][j]<<" ";
+      cout<< matrix[i][j]<<" ";
     }
     cout<<endl;
   }
