cd64f6fdfbe74ef01674c860076661bd3154c614
Bikram Pal <palbikram557@gmail.com>
2026-06-11 13:35:43 +0530

    add: implement linked list with node deletion and traversal functions

---
+++ LinkedList/DeleteInLL.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node
+{
+  public:
+  int data;
+  node * next;
+
+  node(int data1, node* next1)
+  {
+    data = data1;
+    next = next1;
+  }
+  node(int data1)
+  {
+    data = data1;
+    next = nullptr;
+  }
+};
+node * LL(vector<int> arr)
+{
+  node * head = new node(arr[0]);
+  node* mover = head;
+  for(int i=1;i<arr.size(); i++)
+  {
+    node* temp = new node(arr[i]);
+    mover->next = temp;
+    mover = temp;
+  }
+  return head;
+}
+void deleteNode(node  * head, int target)
+{
+  node* p = head;
+  if(head==nullptr)
+  {
+    cout<<"Null of the head"<<"\n";
+    return;
+  }
+  if(head->next== nullptr && head->data == target)
+  {
+    node* temp = head;
+    head = nullptr;
+    delete head;
+    return;
+  }
+  node * cur = head;
+  node *prev = nullptr;
+  while(cur!=nullptr)
+  {
+    if(cur->data==target)
+    {
+      prev ->next = cur->next;
+      delete cur;
+      return;
+    }
+    prev = cur;
+    cur = cur->next;
+  }
+}
+void tarversal(node * head)
+{
+  node * temp = head;
+  while(temp!=nullptr)
+  {
+    cout<<temp->data<<" ";
+    temp = temp->next;
+  }
+  return;
+}
+int main()
+{
+  vector<int> arr = {12,34,45,65};
+  node * head = LL(arr);
+  int target = 34;
+
+  deleteNode(head, target);
+  tarversal(head);
+  return 0;
+}
